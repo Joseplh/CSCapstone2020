9b51b45ef1a14d1b0d0d0a521271ecc4c817f7ee
kzyla <kzyla@unomaha.edu>
2020-10-22 14:39:15 -0500

    add travis.yml

---
+++ Dragons Lair/src/test/java/ControllerTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		assert(true);
+	}
+
+}
